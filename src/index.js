e6e769d2b9abb7f11bef994e8225cc53c21d5e6d
Nayan Joshi <50129883+Nayan-joshi07@users.noreply.github.com>
2023-09-28 21:54:40 +0530

    Implemented the get request for the course id

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.status(200).json(courseData);
 });
+
+app.get('/courses/:courseId', (req, res) => {
+    let airtribeCourses = courseData.airtribe;
+    let courseIdPassed = req.params.courseId;
+    console.log("Req Params", req.params);
+    let filteredCourse = airtribeCourses.filter(val => val.courseId === courseIdPassed);
+    console.log("Filtered Course", filteredCourse);
+    if (filteredCourse.length == 0) {
+        return res.status(404).send("No Appropirate Course Id Found with that id");
+    }
+    return res.status(200).json(filteredCourse);
+});
 app.listen(port, (error) => {
     if (error) {
         console.log("Something went wrong while starting the server");
